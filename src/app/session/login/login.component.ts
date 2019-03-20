28bd9540fdbf91e66cfe637fa67dc13ca4d7d8d7
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2019-03-20 09:12:56 -0500

    Created LoginComponent form with validation and custom styles.

--- src/app/session/login/login.component.ts
+++ src/app/session/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    'userId': new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.pattern(/^[0-9]*$/)
+    ])
+  });
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
+  onSubmit() {
+    console.log(this.form);
   }
 
+  login():void {
+
+  }
 }
--- src/app/session/session.module.ts
+++ src/app/session/session.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatDividerModule } from '@angular/material';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -11,6 +14,14 @@ import { LoginComponent } from './login/login.component';
   declarations: [NotFoundComponent, LoginComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(SessionRoutes),
   ]
 })
